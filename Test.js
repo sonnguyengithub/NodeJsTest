4767d4cb63ffd29770eb0b72bfb58d4061c664c8
sonnguyengithub <tson81@gmail.com>
2017-11-04 21:35:40 +0700

    'aaa'

--- Test.js
+++ Test.js
@@ -2,6 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end('Hello New NodeJs!');
-        //SOn test QA
     }).listen(8080);
 
