61953ccd82b4d2f23058ebfc80be70f8f531f05b
sonnguyengithub <tson81@gmail.com>
2017-11-04 10:53:25 +0700

    ommit test code new

--- Test.js
+++ Test.js
@@ -1,16 +1,16 @@
-// var http = require('http');
-// //Now your application has access to the HTTP module, and is able to create a server:
+var http = require('http');
+//Now your application has access to the HTTP module, and is able to create a server:
+
+http.createServer(function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('Hello New NodeJs!');
+    }).listen(8080);
+
+// var express = require('express')
+// var app = express()
 //
-// http.createServer(function (req, res) {
-//         res.writeHead(200, {'Content-Type': 'text/html'});
-//         res.end('Hello World!');
-//     }).listen(8080);
-
-var express = require('express')
-var app = express()
-
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
-
-app.listen(3000)
+// app.get('/', function (req, res) {
+//     res.send('Hello World')
+// })
+//
+// app.listen(3000)
