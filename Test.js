2342458f00c1a677d7011d5f105a0c5d0fd6d744
sonnguyengithub <tson81@gmail.com>
2017-11-04 17:15:40 +0700

    'editFile'

--- Test.js
+++ Test.js
@@ -1,16 +1,7 @@
 var http = require('http');
-//Now your application has access to the HTTP module, and is able to create a server:
 
 http.createServer(function (req, res) {
-        res.writeHead(200, {'Content-Type': 'text/html'});//aaahhh
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.end('Hello New NodeJs!');
     }).listen(8080);
 
-// var express = require('express')
-// var app = express()
-//
-// app.get('/', function (req, res) {
-//     res.send('Hello World')
-// })
-//
-// app.listen(3000)
